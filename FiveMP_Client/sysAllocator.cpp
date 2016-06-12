73c90e68644df5875f38557c50035b87288b3a52
chmod0644 <chmod_0644@five-multiplayer.net>
2016-06-12 13:18:59 -0300

    Fixed broken function that made the game crash

--- FiveMP_Client/sysAllocator.cpp
+++ FiveMP_Client/sysAllocator.cpp
@@ -25,7 +25,8 @@ namespace rage
 
 static HookFunction hookFunction([]()
 {
-#if 0 //disable temporarily, todo: fix it, shows up twice on ida - 6-12-2016
-	rage::g_tlsOffset = *hook::pattern("B9 ? ? ? ? 48 8B 0C 01 45 33 C9 49 8B D2 48").get(0).get<uint32_t>(1);
-#endif
+	//Glorious copy paste from https://github.com/gallexme/Captain-Hook/blob/master/CaptainHook/Scripting/ScriptEngine.cpp, old signature makes everything crash in 757, unsure on what changed in the game except for a table being different.
+	auto activeThreadTlsOffsetPattern = hook::pattern("48 8B 04 D0 4A 8B 14 00 48 8B 01 F3 44 0F 2C 42 20");
+	rage::g_tlsOffset = *activeThreadTlsOffsetPattern.count(1).get(0).get<uint32_t>(-4);
+	//rage::g_tlsOffset = *hook::pattern("B9 ? ? ? ? 48 8B 0C 01 45 33 C9 49 8B D2 48").get(0).get<uint32_t>(1);
 });
